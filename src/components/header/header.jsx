02a8350182b7bb7fa2795822cc9206584fa99637
idojeyn <idojeyn0405@gmail.com>
2025-06-29 21:43:53 +0500

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import './header.css'
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      lewiorfgl
+    <div className='cont'>
+      <header className='navbar '>
+        <div className="container">
+          <h2><a href="index.html" className='logo'>Whatever</a></h2>
+          <ul className="nav justify-content-end">
+            <div className={`sidebar ${open ? 'active' : ''}`}>
+              <li className="nav-item">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#filter">Filter</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#gallery">Gallery</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href='#Contact'>Contact</a>
+              </li>
+            </div>
+              <li className="nav-item">
+                <a className="nav-link " href="#login">Log in</a>
+              </li>
+          </ul>
+          <div className="hamburger" onClick={() => setOpen(!open)}>
+            <i className="fa-solid fa-bars "></i>
+          </div>
+        </div>
+      </header>
+      <div className="clr"></div>
     </div>
   )
 }
