12c502b564ccfbc0269d5bfc8134494ef1db8ff7
idojeyn <idojeyn0405@gmail.com>
2025-06-30 21:22:59 +0500

    i add userprofil

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,33 +3,68 @@ import React, { useState } from 'react';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className='cont'>
       <header className='navbar '>
         <div className="container">
           <h2><a href="index.html" className='logo'>Whatever</a></h2>
-          <ul className="nav justify-content-end">
-            <div className={`sidebar ${open ? 'active' : ''}`}>
-              <li className="nav-item">
-                <a className="nav-link" href="#home">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#filter">Filter</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#gallery">Gallery</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href='#Contact'>Contact</a>
-              </li>
+          <div className="nav">
+            <ul className="nav justify-content-end">
+              <div className={`sidebar ${open ? 'active' : ''}`}>
+                <li className="nav-item">
+                  <a className="nav-link" href="#home">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#filter">Filter</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#gallery">Gallery</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href='#Contact'>Contact</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href="#login">Log in</a>
+                </li>
+              </div>
+            </ul>
+            <div className="user" onClick={() => setShowMenu(!showMenu)} ><i className="fa-solid fa-user"></i>
+              user
             </div>
-              <li className="nav-item">
-                <a className="nav-link " href="#login">Log in</a>
-              </li>
-          </ul>
-          <div className="hamburger" onClick={() => setOpen(!open)}>
-            <i className="fa-solid fa-bars "></i>
+            <div className={`sub-menu-wrap ${showMenu ? 'active' : ''}`} >
+              <div className="sub-menu">
+                <div className="user-info">
+                  <i className="fa-solid fa-user"></i>
+                  <h3>user-info</h3>
+                </div>
+                <hr />
+                <a href="a" className='sub-menu-link'>
+                  <i className="fa-solid fa-user"></i>
+                  <p>Edit Profil</p>
+                  <span><i className="fa-solid fa-angle-right"></i></span>
+                </a>
+                <a href="a" className='sub-menu-link'>
+                  <i className="fa-solid fa-gear"></i>
+                  <p>Settings & Privasy</p>
+                  <span><i className="fa-solid fa-angle-right"></i></span>
+                </a>
+                <a href="a" className='sub-menu-link'>
+                  <i className="fa-solid fa-circle-question"></i>
+                  <p>Help & Support</p>
+                  <span><i className="fa-solid fa-angle-right"></i></span>
+                </a>
+                <a href="a" className='sub-menu-link'>
+                  <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                  <p>Logout</p>
+                  <span><i className="fa-solid fa-angle-right"></i></span>
+                </a>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="hamburger" onClick={() => setOpen(!open)}>
+          <i className="fa-solid fa-bars "></i>
         </div>
       </header>
       <div className="clr"></div>
